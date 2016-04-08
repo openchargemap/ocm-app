d468ff0e245689a09ff8db35e5a0e18e9bea97f7
webprofusion-chrisc <christopher.cook@webprofusion.com>
2016-04-08 09:45:56 +0800

    Work in progress (image uploads)

---
+++ app/pages/mediaupload/mediaupload.ts
@@ -0,0 +1,22 @@
+import {Page, NavController, Modal, Alert} from 'ionic-angular';
+import {AppManager} from '../../core/ocm/services/AppManager';
+
+@Page({
+    templateUrl: 'build/pages/mediaupload/mediaupload.html'
+})
+export class MediaUploadPage {
+
+
+    constructor(public appManager: AppManager, public nav: NavController) {
+
+    }
+
+    onPageWillEnter() {
+
+      
+       
+    }
+
+    
+
+}
