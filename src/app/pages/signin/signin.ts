174f6bc557c3f29f4c4e0bca295b993923d0604f
webprofusion-chrisc <christopher.cook@webprofusion.com>
2018-10-04 10:40:27 +0800

    WIP: ionic 4 port

--- src/app/pages/signin/signin.ts
+++ src/app/pages/signin/signin.ts
@@ -4,7 +4,7 @@ import { AppManager } from './../../services/AppManager';
 import { TranslateService } from '@ngx-translate/core';
 
 import { Component, NgZone } from '@angular/core';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 import { NavController, NavParams, AlertController, LoadingController } from '@ionic/angular';
 import { UserProfile } from '../../model/UserProfile';
 
@@ -24,7 +24,7 @@ export class SignInPage {
         public loadingController: LoadingController,
         public logging: Logging,
         public router: Router,
-        private location:Location
+        private location: Location
     ) {
         this.email = '';
 
@@ -36,8 +36,8 @@ export class SignInPage {
 
     cancelSignIn() {
         // FIXME: this.nav.pop();
-       this.location.back();
-        }
+        this.location.back();
+    }
 
     async performSignIn() {
 
@@ -48,30 +48,32 @@ export class SignInPage {
 
         // sign in with supplied email address and password
         let signInFailed = false;
-        const signInResult = await this.appManager.api.performSignIn(this.email, this.password).catch(async (err) => {
-            signInFailed = true;
-            // sign in rejected
+
+        try {
+
+            const signInResult = await this.appManager.api.performSignIn(this.email, this.password);
+
             loading.dismiss();
-            const a = await this.alertController.create({
-                header: 'Open Charge Map',
-                subHeader: 'Email or Password not recognised',
-                buttons: ['Ok']
-            });
-            a.present();
-
-            this.logging.log('Error logging in:' + err);
-        });
-
-
-        loading.dismiss();
-
-        if (!signInFailed) {
 
             // signed in OK, save response and return to main app
             localStorage.setItem('authResponse', JSON.stringify(this.appManager.api.authResponse));
 
             // navigation to main app. TODO: navigate to last requested page (route guard)
             this.router.navigateByUrl('/search');
+
+        } catch (err) {
+            signInFailed = true;
+            // sign in rejected
+            loading.dismiss();
+
+            const a = await this.alertController.create({
+                header: 'Open Charge Map',
+                subHeader: 'Email or Password not recognised',
+                buttons: ['Ok']
+            });
+            await a.present();
+
+            this.logging.log('Error logging in:' + err);
         }
 
     }
