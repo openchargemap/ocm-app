fef8f4e2459f5e4ae67547381b6dacb588eddabb
webprofusion-chrisc <christopher.cook@webprofusion.com>
2019-12-01 15:04:33 +0800

    Add config for OCM API Key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,5 +11,6 @@ export const environment = {
   enableStaticMaps: false,
   defaultMapProvider: 4, //1: GM native 4: MapBox // 5:MappingAPI.MAPKIT_JS  //6: mapbox GL with Maptiler,
   apiBase: 'https://api.openchargemap.io/',
+  apiKey:'',
   enabledFeatures: [ 'MAP', 'ADD_COMMENT', 'ADD_PHOTO', 'FAVOURITES', 'FILTER_OPTIONS_BY_COUNTRY']
 };
