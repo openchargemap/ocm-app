b05d5eda9d14daeef356de674b39a11e6b3a773c
webprofusion-chrisc <christopher.cook@webprofusion.com>
2021-11-05 15:41:43 +0800

    v8.1: Angular 13 update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { MappingAPI } from "../app/services/mapping/interfaces/mapping";
 
 export const environment = {
   name: 'prod',
-  version: '8.0.0',
+  version: '8.1.0',
   production: true,
   analyticsId: '',
   googleMapsKey: '',
