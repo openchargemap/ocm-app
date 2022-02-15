6d420919ac735862cdb73310439824d7e0fb4761
webprofusion-chrisc <christopher.cook@webprofusion.com>
2022-02-15 10:11:35 +0800

    Update (downgrade ionic)

---
+++ 4376.ecb7b4f0d46f9da7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{4376:(h,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(5225),c=s(9821),a=s(5951);const n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,c.b)(this);return(0,o.h)(o.H,{class:(0,a.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
