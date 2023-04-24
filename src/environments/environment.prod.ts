e25edf67f405d2f5ad705858ac48d5a61aff035d
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-04-24 14:13:31 +0800

    Update dev version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { MappingAPI } from "../app/services/mapping/interfaces/mapping";
 
 export const environment = {
   name: 'prod',
-  version: '8.5.3',
+  version: '8.5.4',
   production: true,
   analyticsId: '',
   googleMapsKey: '',
