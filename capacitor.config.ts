4dd51afa84173e78f9707177e10a2f7300558773
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-07-06 16:26:56 +0800

    Cleanup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,15 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.webprofusion.openchargemap',
-  appName: 'Open Charge Map',
-  webDir: 'www',
-  bundledWebRuntime: false
-,
-    android: {
-       buildOptions: {
-          keystorePath: 'c:\Work\GIT\ocm-private\Certs\Android\android.keystore',
-          keystoreAlias: 'ocm',
-       }
-    }
-  };
+  appId: "com.webprofusion.openchargemap",
+  appName: "Open Charge Map",
+  webDir: "www",
+  android: {
+    buildOptions: {
+      keystorePath: "c:/Work/GIT/ocm-private/Certs/Android/android.keystore",
+      keystoreAlias: "ocm",
+    },
+  },
+};
 
 export default config;
