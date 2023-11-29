b54bfe5915315c269d766868502191eaabcb973e
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-11-29 12:49:54 +0800

    WIP - sign in/register form

--- src/app/pages/signin/signin.ts
+++ src/app/pages/signin/signin.ts
@@ -13,6 +13,9 @@ import { AlertController, LoadingController, ModalController } from '@ionic/angu
 export class SignInPage {
     email: string;
     password: string;
+    username: string;
+
+    mode: string = 'register';
 
     constructor(
         public appManager: AppManager,
@@ -36,7 +39,52 @@ export class SignInPage {
         this.modalController.dismiss();
     }
 
+    async performRegister() {
+        const loading = await this.loadingController.create({
+            message: 'Signing In..'
+        });
+        await loading.present();
+
+        // sign in with supplied email address and password
+        let signInFailed = false;
+
+        try {
+
+            const registerResult = await this.appManager.api.performRegister(this.username, this.email, this.password);
+
+            loading.dismiss();
+
+            // signed in OK, save response and return to main app
+            localStorage.setItem('authResponse', JSON.stringify(this.appManager.api.authResponse));
+
+            this.appManager.isUserAuthenticated(true);
+
+            // navigation to main app. TODO: navigate to last requested page (route guard)
+            this.modalController.dismiss();
+
+            this.appManager.analytics.appEvent("Profile", "SignedIn");
+
+        } catch (err) {
+            signInFailed = true;
+            // sign in rejected
+            loading.dismiss();
+
+            const a = await this.alertController.create({
+                header: 'Open Charge Map',
+                subHeader: 'Email or Password not recognised',
+                buttons: ['Ok']
+            });
+            await a.present();
+
+            this.logging.log('Error logging in:' + err);
+        }
+
+    }
     async performSignIn() {
+
+        if (this.mode == 'register') {
+            return this.performRegister();
+        }
 
         const loading = await this.loadingController.create({
             message: 'Signing In..'
