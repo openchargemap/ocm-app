9ed6f943b0956bb23b6683cba2f32283dabea258
Christopher Cook <christopher.cook@webprofusion.com>
2026-09-18 16:26:08 +0800

    Update version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { MappingAPI } from "../app/services/mapping/interfaces/mapping";
 
 export const environment = {
   name: 'prod',
-  version: '8.10.0',
+  version: '9.0.0',
   production: true,
   analyticsId: '',
   googleMapsKey: '',
